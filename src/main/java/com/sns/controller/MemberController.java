dfd95e76491243cd8aada4812acc5cab331d472a
amandel <hobulian@gmail.com>
2023-03-14 06:49:28 +0900

    - 쓸모 없는 코드 제거로 code depth 감소 - UniqueId를 서버에서 제작하지 않고, DB에서 담당하게 하여 서버측의 UUID 제작 기능 제거

--- src/main/java/com/sns/controller/MemberController.java
+++ src/main/java/com/sns/controller/MemberController.java
@@ -20,24 +20,14 @@ public class MemberController {
     public ResponseEntity<String> addMember(@RequestBody Member member) {
         boolean isDuplicatedUser = memberService.checkDuplicatedUser(member.getUser_id());
 
-        if (isDuplicatedUser) { // User_id(Alias)의 중복 체크
+        if (isDuplicatedUser) { // User_id의 중복 체크
             return ResponseEntity.badRequest().body("이미 존재하는 ID입니다.");
-        } else {
-            member.builder().id(generateUniqueId()); // 서버측에 저장될 실제 유저의 ID를 저장
-            memberService.addMember(member);
-            return ResponseEntity.ok("회원 가입을 완료했습니다.");
         }
+
+        memberService.addMember(member);
+        return ResponseEntity.ok("회원 가입을 완료했습니다.");
     }
 
-    private String generateUniqueId() { // 서버측에 저장할 Unique ID를 DB에서 겹치지 않을 때까지 재귀적으로 생성
-        String uuid = UUID.randomUUID().toString();
-        boolean isDuplicatedID = memberService.checkDuplicatedID(uuid);
-
-        if (isDuplicatedID) {
-            uuid = generateUniqueId();
-        }
-        return uuid;
-    }
 
     @GetMapping("/isDuplicated")
     public ResponseEntity<String> checkDuplicatedMember(@RequestParam String user_id) {
