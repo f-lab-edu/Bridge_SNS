ba45dc426c0a1fd361e47d5791fbcfc773f530ce
amandel <hobulian@gmail.com>
2023-03-21 01:55:49 +0900

    - 변경된 MemberController에 대해서 기존의 Unit Test 변경

--- src/test/java/com/sns/sns_bridge/SnsBridgeApplicationTests.java
+++ src/test/java/com/sns/sns_bridge/SnsBridgeApplicationTests.java
@@ -2,20 +2,9 @@ package com.sns.sns_bridge;
 
 import com.sns.config.MybatisConfig;
 import com.sns.controller.MemberController;
+import com.sns.exceptions.DuplicatedUserIdException;
 import com.sns.model.Member;
 import com.sns.service.MemberService;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.*;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,11 +12,19 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.sql.SQLException;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class SnsBridgeApplicationTests {
@@ -72,10 +69,9 @@ class MemberControllerTest {
     @Test
     @DisplayName("회원 가입 성공 테스트")
     public void addMember_success() {
-        // when()안의 메소드가 호출될 때, thenReturn(boolean)으로 메소드의 결과값을 가정할 수 있다
-        when(memberService.checkDuplicatedUserID(member.getUser_id())).thenReturn(false);
         // 테스트에서 실제 테스트하지 않아도 되는 메소드 호출 부분을 doNothing 처리
-        doNothing().when(memberService).addMember(member);
+        doNothing().when(memberService).checkDuplicatedUserID(member.getUser_id());
+        doNothing().when(memberService).addMemberWithUUID(member);
 
         ResponseEntity<String> response = memberController.addMember(member);
 
@@ -86,29 +82,23 @@ class MemberControllerTest {
     @Test
     @DisplayName("userID가 이미 존재하는 경우의 회원 가입 실패 테스트")
     public void addMember_userIdExists() {
-        when(memberService.checkDuplicatedUserID(member.getUser_id())).thenReturn(true);
+        doThrow(new DuplicatedUserIdException()).when(memberService).checkDuplicatedUserID(member.getUser_id());
 
-        ResponseEntity<String> response = memberController.addMember(member);
-
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
-        assertEquals("이미 존재하는 ID입니다.", response.getBody());
+        assertThrows(DuplicatedUserIdException.class, () -> memberController.addMember(member));
     }
 
     @Test
     @DisplayName("중복된 userID가 존재할 경우의 API 테스트")
     public void checkDuplicatedMember_duplicated() {
-        when(memberService.checkDuplicatedUserID(member.getUser_id())).thenReturn(true);
+        doThrow(DuplicatedUserIdException.class).when(memberService).checkDuplicatedUserID(member.getUser_id());
 
-        ResponseEntity<String> response = memberController.checkDuplicatedMember(member.getUser_id());
-
-        assertEquals(HttpStatus.CONFLICT, response.getStatusCode());
-        assertEquals("이미 존재하는 ID입니다.", response.getBody());
+        assertThrows(DuplicatedUserIdException.class, () -> memberController.checkDuplicatedMember(member.getUser_id()));
     }
 
     @Test
     @DisplayName("중복된 userID가 존재하지 않을 경우의 API 테스트")
     public void checkDuplicatedMember_notDuplicated() {
-        when(memberService.checkDuplicatedUserID(member.getUser_id())).thenReturn(false);
+        doNothing().when(memberService).checkDuplicatedUserID(member.getUser_id());
 
         ResponseEntity<String> response = memberController.checkDuplicatedMember(member.getUser_id());
 
