1ce7fd90bd1e39201a9add6188b85b27f0f5f5f7
amandel <hobulian@gmail.com>
2023-04-11 02:48:53 +0900

    - argon2 알고리즘을 bcrypt 알고리즘으로 변경

--- src/main/java/com/sns/util/PasswordEncryptor.java
+++ src/main/java/com/sns/util/PasswordEncryptor.java
@@ -1,22 +1,20 @@
 package com.sns.util;
 
-import de.mkammerer.argon2.Argon2;
-import de.mkammerer.argon2.Argon2Factory;
-import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@RequiredArgsConstructor
 public class PasswordEncryptor {
-    private final Argon2 argon2;
+    private final PasswordEncoder passwordEncoder;
 
     public PasswordEncryptor() {
-        argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public String hashPassword(String password) {
-        return argon2.hash(2, 65536, 1, password.toCharArray());
+        return passwordEncoder.encode(password);
     }
 
     public boolean verifyPassword(String password, String hashedPassword) {
-        return argon2.verify(hashedPassword, password.toCharArray());
+        return passwordEncoder.matches(password, hashedPassword);
     }
 }
