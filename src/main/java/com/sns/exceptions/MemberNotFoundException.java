e889a44e830a2cae21e936f472e73e9bcd685673
amandel <hobulian@gmail.com>
2023-07-10 21:10:16 +0900

    - Typo 수정

--- src/main/java/com/sns/exceptions/MemberNotFoundException.java
+++ src/main/java/com/sns/exceptions/MemberNotFoundException.java
@@ -3,7 +3,7 @@ package com.sns.exceptions;
 public class MemberNotFoundException extends RuntimeException {
     private static final String MESSAGE = "사용자 정보를 찾을 수 없습니다";
 
-    public MemberNotFoundException() { // 틀린 비밀번호 예외
+    public MemberNotFoundException() { // 사용자 정보를 찾을 수 없는 예외
         super(MESSAGE);
     }
 }
